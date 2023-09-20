7e553a5e8e6377ef1ec1b29eda81b64a8e4c5ea6
Jemo <jamesshisia6@gmail.com>
2023-09-20 05:57:07 +0300

    Rest Controller Access endpoint

---
+++ src/main/java/dev/nebucodenezzar/sociallogin/Controllers/SocialController.java
@@ -0,0 +1,16 @@
+package dev.nebucodenezzar.sociallogin.Controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SocialController {
+    @GetMapping("/")
+    public String home(){
+        return "Hello,Welcome home";
+    }
+    @GetMapping("/secured")
+    public String secured(){
+        return "Hello,Welcome to secured";
+    }
+}
