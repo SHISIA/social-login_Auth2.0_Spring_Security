72488762a20252d1d9bf5b0384220b0951eebafb
Jemo <jamesshisia6@gmail.com>
2023-09-20 23:08:43 +0300

    Auth 2 Rest controller

--- src/main/java/dev/nebucodenezzar/sociallogin/Controllers/SocialController.java
+++ src/main/java/dev/nebucodenezzar/sociallogin/Controllers/SocialController.java
@@ -3,6 +3,8 @@ package dev.nebucodenezzar.sociallogin.Controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 @RestController
 public class SocialController {
     @GetMapping("/")
